d663cf0493a6282a9090f4c89fc5721828385fc9
LawKarma <mathisbourgoin94@gmail.com>
2024-01-30 14:02:40 +0100

    small small small update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Form, Route, Routes } from 'react-router-dom';
 import { Accueil } from './views/Accueil/Accueil';
 import { NavBar } from './views/Navbar';
 function App() {
--- src/views/Accueil/Accueil.js
+++ src/views/Accueil/Accueil.js
@@ -1,8 +1,11 @@
-import logo from '../../Logoeqo.png'
-
+import "./Accueil.css"
 export function Accueil(){
-  return  <div>
-    bjr
-  </div>
-}
+  return <div className='Accueilbody'>
+    <div className="Text">
+    <h1 className="AccueilTitle">Bienvenue</h1>
+      <p className="AccueilText">Découvrez notre nouvelle game de pièces 
+        que vous pouvez directement comander chez vous </p>
+        </div>
+    </div>
+   }
 
